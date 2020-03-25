d262575c2768e1ebdfa0d299a34b790f0d8db60a
themichaelcaruso <mcaruso@anchron.com>
2020-03-25 05:39:45 -0400

    Trying and failing at random terrain...

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,7 @@ io.set('authorization', function (handshakeData, callback) {
 io.on('connection', function(socket: Socket){
   console.log('Connection');
   socket.on('login', function(){
-      socket.emit('chunk', randomChunk(100, 100));
+      socket.emit('chunk', lakes(100, 100));
   });
   socket.on('disconnect', function(){
     console.log('Quit.');
@@ -38,6 +38,35 @@ http.listen(3000, function() {
   console.log('listening on *:3000');
 });
 
+function lakes(width : number, height : number) : Chunk {
+  var tiles : number[][] = [];
+  for(var x = 0; x<width; x++) {
+    tiles[x] = [];
+    for(var y = 0; y<height; y++) {
+      var random = Math.random();
+      random += touching(x,y,tiles)/10;
+
+      tiles[x][y] = random > .9 ? 1 : 0;
+    }
+  }
+  return new Chunk(tiles, []);
+}
+function touching(i : number,  j : number, tiles : number[][]) : number {
+    var count = 0;
+   if(j != 0)  // to the top
+     if(tiles[i][j-1] == 1) count++;
+   if(i != 0)
+     if(tiles[i-1][j] == 1) count++;
+   if(j!=0 && i!=0)
+     if(tiles[i-1][j-1] == 1) count++;
+   if(i!=tiles.length-1)
+       if(tiles[i+1][j] == 1) count+=2;
+
+  if(count == 1) {
+    count = 3;
+  }
+  return count;
+}
 function randomChunk(width : number, height : number) : Chunk {
   var tiles : number[][] = [];
   for(var x = 0; x<width; x++) {
